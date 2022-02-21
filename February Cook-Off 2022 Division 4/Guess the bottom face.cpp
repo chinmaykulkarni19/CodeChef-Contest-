f6db949c3059b50aef83ae1cfb384614a5b1dd23
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-02-21 21:56:55 +0530

    Add files via upload

---
+++ February Cook-Off 2022 Division 4/Guess the bottom face.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int X;
+        cin >> X;
+        cout << (7 - X) << endl;
+    }
+    return 0;
+}
