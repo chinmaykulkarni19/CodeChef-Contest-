a91ae98620b60bac1f979cc084be7044ffed96ec
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-02-23 22:41:46 +0530

    Add files via upload

---
+++ Too many items.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int l = ceil(n / 10.0);
+        cout << l << endl;
+    }
+    return 0;
+}
