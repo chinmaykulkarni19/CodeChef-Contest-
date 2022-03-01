4b89703c33b36858392394b78460289e739ce907
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-03-01 21:39:26 +0530

    Add files via upload

---
+++ February Lunchtime 2022 Division 4/Watching Movies at 2x.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    int t = y / 2;
+    int u = x - y;
+    int v = t + u;
+    cout << v << endl;
+    return 0;
+}
