e0911b68d6984d62258dac8e7c2a32643b2d5f82
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-03-01 21:39:04 +0530

    Add files via upload

---
+++ February Lunchtime 2022 Division 4/Good Program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n % 4 == 0)
+            cout << "Good" << endl;
+        else
+            cout << "Not Good" << endl;
+    }
+    return 0;
+}
