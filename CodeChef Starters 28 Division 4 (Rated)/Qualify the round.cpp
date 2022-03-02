1356fd330d608fe777a022e61864530f145c455b
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-03-02 21:51:14 +0530

    Add files via upload

---
+++ CodeChef Starters 28 Division 4 (Rated)/Qualify the round.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, a, b;
+        cin >> x >> a >> b;
+        if (a * 1 + b * 2 == x)
+            cout << "Qualify" << endl;
+        else if ((a * 1 + b * 2) < x)
+            cout << "NotQualify" << endl;
+        else
+            cout << "Qualify" << endl;
+    }
+    return 0;
+}
