a44d9ba5916714e5d4a7b1be86ec2e17d51cab15
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-03-02 21:51:51 +0530

    Add files via upload

---
+++ CodeChef Starters 28 Division 4 (Rated)/Self Defence Training.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] >= 10 && a[i] <= 60)
+                count++;
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
